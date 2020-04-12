4d75b70dac326ac86857a13481c5c7525cc10e26
Alexander Flores <aflores@mobilityware.com>
2020-04-11 21:29:12 -0700

    Initial Directory Setup
    *Updated ReadMe
    *Added empty Setup.bat
    *Added windows entry point

---
+++ Engine/main.cpp
@@ -0,0 +1,10 @@
+/* =============================================================================
+Resources:
+https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-winmain
+==============================================================================*/
+#include <windows.h>
+
+int __clrcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# OdinEngine
-A game engine based off the handmade hero series
+# Odin Engine
+=============
+
+Welcome to the Odin Engine source code!
+
+Getting started
+----------------------
+
+### Windows
+
+1. Install **[GitHub for Windows](https://windows.github.com/)** then clone this repository.
+
+2. Install **Visual Studio 2019**.
